e0a1744c3221319a72445a1df2f374b6e47c3024
freshposh-code <atomisefarouk919@gmail.com>
2024-04-27 03:01:46 +0100

    Add: New file

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,8 @@ export { default as Topbar } from './Topbar/Top'
 export { default as Stats } from './Revenue/Stats'
 export { default as Chart } from './Charts/Index'
 export { default as TimeTrack } from './TimeTrack/Track'
-export { default as Transaction } from './Payment/Transaction'    
+export { default as Transaction } from './Payment/Transaction'
+
+// MOBILE EXPORTS
+
+export { default as MobileNav } from './Navbar/MobileNav'
