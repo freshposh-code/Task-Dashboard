8f83a1510e84f645d767dbaca6d5b29ed38d9ad0
freshposh-code <atomisefarouk919@gmail.com>
2024-04-27 03:01:26 +0100

    Add: new assest

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import profile from './profile.jpg'
 import profileII from './profileII.jpg'
+import credit from './creditcard.png'
 
-export { profile, profileII }
+export { profile, profileII, credit }
