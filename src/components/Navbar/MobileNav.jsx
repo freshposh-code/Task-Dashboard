483706798daa3ae6c6c86d80bb33700873b0f07f
freshposh-code <atomisefarouk919@gmail.com>
2024-04-28 20:45:10 +0100

    commit changes

--- src/components/Navbar/MobileNav.jsx
+++ src/components/Navbar/MobileNav.jsx
@@ -10,12 +10,12 @@ const MobileNav = () => {
             <section className='w-screen'>
                 <div className='md:hidden flex bg-[#2e25ae] xm:p-3 p-2 items-end rounded-[40px]'>
 
-                    <div className='relative flex '>
+                    <div className='relative flex justify-between w-screen'>
                         {sidebarIcons.map((item, index) => (
                             <div key={item.icon}
                                 onClick={() => handleClick(index)}
                                 className={`cursor-pointer rounded-[15px] ${activeIndex === index ? 'bg-white text-[#ff0000ce] sideshadow absolute left-[1rem] -top-[90%] transform -translate-x-[21px]' : 'text-[#7e75ff]'} p-3`}>
-                                <span>{item.icon}</span>
+                                <div>{item.icon}</div>
                             </div>
                         ))}
                     </div>
