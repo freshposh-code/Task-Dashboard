5d41a1be2a901cc94acc4a59c5c53a1c28be8ac1
freshposh-code <atomisefarouk919@gmail.com>
2024-04-28 20:15:01 +0100

    wrap: State management

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
+import { StateContextProvider } from './Context/StateContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
+  <StateContextProvider>
     <App />
-  </React.StrictMode>,
+  </StateContextProvider>
 )
